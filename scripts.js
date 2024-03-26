f675c68d1eebb478096c98da71b1b85ed5f91bce
Raja Ambedkar Bandili <101200950+RajaAmbedkar@users.noreply.github.com>
2024-03-26 08:03:16 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Add a custom scrolling behavior to the navigation links
-const links = document.querySelectorAll('nav a');
-links.forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.hash);
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
